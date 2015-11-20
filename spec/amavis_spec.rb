929e5c91820ea57b845640d4fc37ac765a091ee2
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2015-11-20 12:21:44 +0100

    verdere testen

--- spec/amavis_spec.rb
+++ spec/amavis_spec.rb
@@ -132,22 +132,55 @@ describe "the extended amavis grok pattern" do
     it "should have the correct elapsedtime" do
       @match.should have_logstash_field("elapsedtime").with_value("1958")
     end
+  end
+
+  describe "with a spammy amavis log line" do
+    before do
+      log_line = 'Mar 15 15:16:24 mcheck4 amavis[18588]: (18588-47) Passed SPAMMY, DEFAULT [93.174.66.65] [93.174.66.65] <a6g6h@a6g6h.r39.it> -> <bart.vanderstraeten@ugent.be>, Message-ID: <201303151516230534@311470.623.2153.16768>, mail_id: 9I671lxS3H2g, Hits: 10.632, size: 23466, queued_as: D1BDE9E332, Subject: "=?UTF-8?Q?profiteer_nu_van_de_daling_van_onze_tarieven?=", From: "=?UTF-8?Q?Auxifina_Kredieten_door_webvoordelen?="_<jan.veyt@webvoordelen.com>, Tests: [CMAE_1=10,DKIM_SIGNED=0.1,DKIM_VALID=-0.1,HS_INDEX_PARAM=0.023,HTML_IMAGE_RATIO_04=0.61,HTML_MESSAGE=0.001,RCVD_IN_DNSWL_NONE=-0.0001,SPF_HELO_PASS=-0.001,SPF_PASS=-0.001], shortcircuit=no, autolearn=disabled, 681 ms'
+      @match = @grok.match(log_line)
+    end
+
+    it "should have the correct action value" do
+      @match.should have_logstash_field("action").with_value("Passed")
+    end
+
+    it "should have the correct ccat value" do
+      @match.should have_logstash_field("ccat").with_value("SPAMMY")
+    end
+
+  end
+
+  describe "with a blocked amavis log line" do
+    before do
+      log_line = 'Mar 15 15:55:12 mcheck4 amavis[20996]: (20996-19) Blocked SPAM, DEFAULT [82.57.200.97] [172.129.36.105] <info@yahoo.com.tw> -> <e.e@ugent.be>,<m.m@ugent.be>, Message-ID: <5123901C01A43BDE@smtp201.alice.it>, mail_id: eA4yT5RIEyFP, Hits: 27.617, size: 1487, Subject: "Re:Youve won", From: "Yahoo_Asia"<info@yahoo.com.tw>, X-Mailer: Microsoft_Outlook_Express_6.00.2800.1081, Tests: [CMAE_1=10,DKIM_ADSP_CUSTOM_MED=0.001,FAKE_REPLY_C=0.001,FILL_THIS_FORM_LONG=3.476,FORGED_MUA_OUTLOOK=2.785,FROM_MISSP_MSFT=1,MISSING_HEADERS=1.207,MONEY_FROM_MISSP=1,NML_ADSP_CUSTOM_MED=1.2,RCVD_IN_DNSWL_NONE=-0.0001,REPLYTO_WITHOUT_TO_CC=1.946,T_FILL_THIS_FORM=0.01,T_FROM_MISSPACED=0.01,T_LOTS_OF_MONEY=0.01,US_DOLLARS_3=2.523,XMAILER_MIMEOLE_OL_1ECD5=2.448], shortcircuit=no, autolearn=disabled, 204 ms'
+      @match = @grok.match(log_line)
+    end
+
+    it "should have the correct action value" do
+      @match.should have_logstash_field("action").with_value("Blocked")
+    end
+
+    it "should have the correct ccat value" do
+      @match.should have_logstash_field("ccat").with_value("SPAM")
+    end
+
+  end
+
+  describe "with a bad header amavis log line" do
+    before do
+      log_line = 'Mar 10 12:54:59 mcheck4 amavis[19543]: (19543-59) Passed BAD-HEADER, DEFAULT [128.178.224.219] [128.178.131.141] <www-data@stisrvm11.epfl.ch> -> <e.e@ugent.be>, Message-ID: <1ac98b88dbb9a38a11f83e8446e9b897@lepfl.ch>, mail_id: RMRzRAG1iLLS, Hits: -1.194, size: 5859, queued_as: 1CA3B7FDC9, Subject: "subject"", From: "www-data"_<www-data@stisrvm11.epfl.ch>, X-Mailer: PHPMailer_5.1_(phpmailer.sourceforge.net), Tests: [HTML_MESSAGE=0.001,MIME_HTML_ONLY=1.105,RCVD_IN_DNSWL_MED=-2.3], shortcircuit=no, autolearn=disabled, 348 ms'
+      @match = @grok.match(log_line)
+    end
+
+    it "should have the correct action value" do
+      @match.should have_logstash_field("action").with_value("Passed")
+    end
+
+    it "should have the correct ccat value" do
+      @match.should have_logstash_field("ccat").with_value("BAD-HEADER")
+    end
 
 
-
-#    it "should have the correct agent" do
-#      @match.should have_logstash_field("agent").with_value("\"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\"")
-#    end
-#
-#    it "should have the correct status code" do
-#      @match.should have_logstash_field("response").with_value("200")
-#    end
-#
-#    it "should have the performance data collected" do
-#      @match.should have_logstash_field("request_time").with_value("0.421")
-#      @match.should have_logstash_field("upstream_response_time").with_value("0.418")
-#      @match.should have_logstash_field("gzip_ratio").with_value("3.28")
-#    end
   end
 
 end
